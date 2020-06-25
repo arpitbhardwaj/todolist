3a871adbc099588df8efdfcce72c0ed4204050ee
Arpit Bhardwaj <arpit.bhardwaj@sap.com>
2020-06-25 11:32:51 +0530

    Initial commit

---
+++ src/test/java/com/ab/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ab.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
